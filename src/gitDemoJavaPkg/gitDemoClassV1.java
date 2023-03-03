a5a6a0dbc514b8b88a88baf5286442168f8c83c1
AmbikaSwamy <ambikadevib@gmail.com>
2023-03-02 22:28:06 -0500

    Ambika added comments in V1 file

--- src/gitDemoJavaPkg/gitDemoClassV1.java
+++ src/gitDemoJavaPkg/gitDemoClassV1.java
@@ -5,9 +5,15 @@ public class gitDemoClassV1
 	public static void main(String[] args) 
 	{
 		System.out.println("Say Hello to Git !");
-
+		
+		// Commenting these lines and merging to central repo.
+		
+		/* Comment STARTS here.
+		
 		System.out.println("Hiii");
-		System.out.println("Ambika says hi !!");			
+		System.out.println("Ambika says hi !!");
+		
+		Comment END here */			
 
 	}
 }
